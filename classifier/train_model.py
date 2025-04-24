cde133d077833690b2f3515670885eba1b94e4ba
lucas <lucascatchlove@gmail.com>
2025-04-24 00:14:35 -0400

    initial commit: added react frontend, flask backend, and the start of my random forest classifier

---
+++ classifier/train_model.py
@@ -0,0 +1,40 @@
+from sklearn.model_selection import train_test_split
+import sklearn.ensemble
+import pandas as pd
+
+
+dataset = pd.read_csv('data/dataset_full.csv')
+
+#feature labels and number of features for later use
+feature_names = list(dataset.columns.values)[:111]
+num_features = len(feature_names)
+
+#split the csv into training data and labels
+X, y = dataset.iloc[:,0:111], dataset['phishing']
+
+#split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+#creation of validation sets for use with feature permutation from the training sets
+X_train, X_vali, y_train, y_vali = train_test_split(X_train, y_train, test_size=0.2)
+
+#train the model
+forest = sklearn.ensemble.RandomForestClassifier(random_state=0).fit(X_train, y_train);
+
+#training predictions and accuracy
+y_pred = forest.predict(X_train)
+
+training_accuracy = sklearn.metrics.accuracy_score(y_train, y_pred)
+
+#test predictions and accuracy
+y_pred = forest.predict(X_test)
+
+test_accuracy = sklearn.metrics.accuracy_score(y_test, y_pred)
+
+print('{:.2%} training accuracy'.format(training_accuracy))
+print('{:.2%} training error'.format(1-training_accuracy))
+print('{:.2%} test accuracy'.format(test_accuracy))
+print('{:.2%} test error'.format(1-test_accuracy))
+
+#for plotting purposes
+forest_acc = [training_accuracy*100, test_accuracy*100]
